0cafeec24a49d22a4a48d4a78958210bfc7d18bc
kgw <kimgw1386@gmail.com>
2024-03-21 20:59:31 +0900

    add model so we can use variable at mustache

--- src/main/java/com/example/demo/controller/FirstController.java
+++ src/main/java/com/example/demo/controller/FirstController.java
@@ -1,12 +1,23 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
     @GetMapping("/hi")
-    public String niceToMeetYou(){
+    public String niceToMeetYou(Model model){
+        // 모델을 추가하면, 모델에 에트리뷰트를 추가해서 html상에 나타낼 수 있음.
+        model.addAttribute("username", "kgw");
         return "greeting";
     }
+    @GetMapping("/bye")
+    public String seeYouNextTime(Model model){
+        // 모델을 추가하면, 모델에 에트리뷰트를 추가해서 html상에 나타낼 수 있음.
+        model.addAttribute("username", "kgw");
+        return "goodbye";
+    }
 }
+
+// 질문점: mustache파일은 무엇인가?
