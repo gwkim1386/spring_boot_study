7f379e9b121555eb95181a3a118bf6bb42d49813
kgw <kimgw1386@gmail.com>
2024-03-22 16:07:35 +0900

    2장까지 끝. MVC정도

--- src/main/java/com/example/demo/controller/FirstController.java
+++ src/main/java/com/example/demo/controller/FirstController.java
@@ -16,8 +16,10 @@ public class FirstController {
     public String seeYouNextTime(Model model){
         // 모델을 추가하면, 모델에 에트리뷰트를 추가해서 html상에 나타낼 수 있음.
         model.addAttribute("username", "kgw");
+        // 이 위 코드를 보면 model 즉 데이터 관련된 모델 객체 통해서 username에 kgw갖다 넣을 수 있음.
         return "goodbye";
     }
 }
 
 // 질문점: mustache파일은 무엇인가?
+// 정리: MVC 패턴은 모델 뷰 컨트롤러 패턴을 말한다. 뷰 템플릿으로 동일한 화면에, 모델을 통해 각 유저들의 데이터를 받아와서, 컨트롤러는 그걸 가져오라고 오더한다.
